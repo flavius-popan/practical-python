b7fcf605b62f08b54f9e43f1c18beb4a7d283d04
Flavius Popan <flaviuspopan@gmail.com>
2022-02-27 12:15:09 -0700

    Finished 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,11 +7,20 @@ rate = 0.05
 BASE_PAYMENT = 2684.11
 total_paid = 0.0
 month = 0
+# User configurable
+extra_payment = 1000
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 
 while principal > 0:
-    payment = BASE_PAYMENT + 1000 if month < 12 else BASE_PAYMENT
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        payment = BASE_PAYMENT + extra_payment
+    else:
+        payment = BASE_PAYMENT
     principal = principal * (1 + rate / 12) - payment
     total_paid = total_paid + payment
     month += 1
+    print(month, total_paid, principal)
 
-print("Total paid", round(total_paid, 4), "in", month, "months")
+print("Total paid", round(total_paid, 4))
+print("Months", month)
